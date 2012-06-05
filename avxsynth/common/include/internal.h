8e26bb4658a48a85c03077371e50f6c408e4c8e3
Avxsynth Pirate <avxsynthPirate@gmail.com>
2012-06-04 18:05:16 -0700

    Fixing bug#23 - unresolved symbol when -O1 or -O2 option is used to compile libavxsynth

--- avxsynth/common/include/internal.h
+++ avxsynth/common/include/internal.h
@@ -66,9 +66,6 @@ struct AVSFunction {
   void* user_data;
 };
 
-
-int RGB2YUV(int rgb);
-
 PClip Create_MessageClip(const char* message, int width, int height,
   int pixel_type, bool shrink, int textcolor, int halocolor, int bgcolor,
   IScriptEnvironment* env);
@@ -120,6 +117,21 @@ static __inline BYTE ScaledPixelClip(int i) {
   return PixelClip((i+32768) >> 16);
 }
 
+static __inline int RGB2YUV(int rgb) 
+{
+  const int cyb = int(0.114*219/255*65536+0.5);
+  const int cyg = int(0.587*219/255*65536+0.5);
+  const int cyr = int(0.299*219/255*65536+0.5);
+
+  // y can't overflow
+  int y = (cyb*(rgb&255) + cyg*((rgb>>8)&255) + cyr*((rgb>>16)&255) + 0x108000) >> 16;
+  int scaled_y = (y - 16) * int(255.0/219.0*65536+0.5);
+  int b_y = ((rgb&255) << 16) - scaled_y;
+  int u = ScaledPixelClip((b_y >> 10) * int(1/2.018*1024+0.5) + 0x800000);
+  int r_y = (rgb & 0xFF0000) - scaled_y;
+  int v = ScaledPixelClip((r_y >> 10) * int(1/1.596*1024+0.5) + 0x800000);
+  return ((y*256+u)*256+v) | (rgb & 0xff000000);
+}
 
 static __inline bool IsClose(int a, int b, unsigned threshold) 
   { return (unsigned(a-b+threshold) <= threshold*2); }
